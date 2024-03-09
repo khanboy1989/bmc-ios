9ebbecc7066fc905b5ca4f81bafbcaecc1a9ee5e
Serhan Khan <khanboy0689@gmail.com>
2024-03-09 23:34:09 +0100

    feat: login view package created

--- Bmc/Application/BmcApp.swift
+++ Bmc/Application/BmcApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BmcApp: App {
     var body: some Scene {
         WindowGroup {
-            Text(PlistFiles.apiStageBaseURL)
+            ContentView()
         }
     }
 }
---
+++ Features/Login/Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Login",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Login",
+            targets: ["Login"]),
+    ],
+    dependencies: [
+        .package(path: "../Core/CommonUI"),
+        .package(path: "../Foundation/Network"),
+        .package(path: "../Foundation/Router")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Login",
+            dependencies: [
+                "Network",
+                "CommonUI",
+                "Router"
+            ]
+        ),
+        .testTarget(
+            name: "LoginTests",
+            dependencies: ["Login"]),
+    ]
+)
