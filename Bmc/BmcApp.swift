05202f2e6811c1f0fb50261f130909825ef17586
Serhan Khan <khanboy0689@gmail.com>
2024-03-09 21:23:09 +0100

    initial commit

---
+++ Bmc/BmcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmcApp.swift
+//  Bmc
+//
+//  Created by Serhan Khan on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BmcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
