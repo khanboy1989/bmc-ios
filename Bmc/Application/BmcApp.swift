e7fb400bf66afc40c48b21e59693445ae06d6f8c
Serhan Khan <khanboy0689@gmail.com>
2024-03-15 08:33:01 +0100

    fix: init of the config class

--- Bmc/Application/BmcApp.swift
+++ Bmc/Application/BmcApp.swift
@@ -14,18 +14,10 @@ struct BmcApp: App {
     let configuration: Configuration
     
     init() {
-        if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
-            print("Bundle display name = \(name)")
-        }
-        let logger = Logger(label: Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "BMC")
-        
-        let apiClientService = APIClientService(logger: logger, configuration: .init(baseURL: URL(string: PlistFiles.apiBaseUrl),
-                                                                                     baseHeaders: ["User-agent": "iOS", "X-API-KEY":"\(PlistFiles.apiKey)",
-                                                                                                   "content-type": "application/json", "Accept-Language": "en"]))
-        configuration = .init(logger: logger, apiClientService: apiClientService)
+        //inherits the configuration for start
+        self.configuration = Configuration()
     }
     
-  
     var body: some Scene {
         WindowGroup {
             LoginMainCoordinator()
--- Bmc/Configuration/Configuration.swift
+++ Bmc/Configuration/Configuration.swift
@@ -11,14 +11,21 @@ import Network
 import Router
 
 
+/// Configuration file that instantiate the ApiClient Service.
 class Configuration: ObservableObject {
-    let logger: ILogger
-    let apiClientService: IAPIClientService
+    private let logger: ILogger
+    private let apiClientService: IAPIClientService
     
-    init(logger: ILogger, apiClientService: IAPIClientService) {
+    init() {
+        
+        let logger = Logger(label: Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "BMC")
+
+        let currentLanguageCode = Locale.current.language.languageCode?.identifier ?? "tr"
+        
+        let apiClientService = APIClientService(logger: logger, configuration: .init(baseURL: URL(string: PlistFiles.apiBaseUrl),
+                                                                                     baseHeaders: ["User-agent": "iOS", "X-API-KEY":"\(PlistFiles.apiKey)",
+                                                                                                   "content-type": "application/json", "Accept-Language": currentLanguageCode]))
         self.logger = logger
         self.apiClientService = apiClientService
     }
-    
-    
 }
